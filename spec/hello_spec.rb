161d336aca25792c97c53818771758a89f18a269
kwgch <m.kwgch@gmail.com>
2015-01-03 16:04:39 +0900

    使えるRSpec入門・その2

--- spec/hello_spec.rb
+++ spec/hello_spec.rb
@@ -1,12 +1,3 @@
-# require_relative '../lib/hello'
-
-# describe Hello do
-#   it "message return hello" do
-#     expect(Hello.new.message).to eq "hello"
-#   end
-# end
-
-# require_relative '../lib/hello'
 require 'spec_helper'
 
 describe 'Hello' do
@@ -28,4 +19,40 @@ describe '四則演算' do
   end
 end
 
+describe 'matcher sample' do
+  it '1 + 2 は ３ になること' do
+    expect(1 + 2).to eq 3
+  end
+  it '[1, 2, 3] は 2 を含むこと' do
+    expect([1, 2, 3]).to include 2
+  end
+  it '[] は 空であること' do
+    expect([]).to be_empty
+  end
 
+  it 'nil でなければ true とみなされること' do
+    expect(1).to be_truthy
+  end
+  it 'nil であれば false とみなされること' do
+    expect(nil).to be_falsey
+  end
+#   it 'error1' do
+#     expect(1).to eq true
+#   end
+#   it 'error2' do
+#     expect(nil).to eq false
+#   end
+
+  it 'pop で 1つ減ること' do
+    x = [1, 2, 3]
+    expect{ x.pop }.to change{ x.size }.from(3).to(2)
+  end
+  it 'push で 1つ増えること' do
+    x = [1, 2, 3]
+    expect{ x.push 10 }.to change{ x.size }.from(3).to(4)
+  end
+
+  it '0で除算するとエラーが起きること' do
+    expect{ 1 / 0 }.to raise_error ZeroDivisionError
+  end
+end
